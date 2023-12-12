51ca9ffcf5a9d5266c1a59c9da480e4bccdf5625
sugar <engineerzjl@foxmail.com>
2023-12-12 21:55:40 +0800

    fix: Fix concatenation bug in crypto.mjs

--- packages/4/other/crypto.mjs
+++ packages/4/other/crypto.mjs
@@ -16,7 +16,7 @@ const password = crypto.randomBytes(32) // 生成随机 32 字节的密码
 const iv = crypto.randomBytes(16) // 生成随机 16 字节的向量
 
 // 待加密的数据
-const data = 'Hello, World!'
+const data = 'Hello, World!Hello, World!Hello, World!Hello, World!Hello, World!'
 console.log('Original data:', data)
 
 // 创建加密算法实例
@@ -25,7 +25,7 @@ const cipher = crypto.createCipheriv(algorithm, password, iv)
 // 使用 update 方法对数据进行加密
 let encrypted = cipher.update(data, 'utf8', 'hex')
 // 加密后的数据以十六进制形式(即字符串)返回
-encrypted = cipher.final('hex')
+encrypted += cipher.final('hex')
 
 console.log('Encrypted data:', encrypted)
 
@@ -35,7 +35,7 @@ const decipher = crypto.createDecipheriv(algorithm, password, iv)
 // 使用 update 方法对数据进行解密
 let decrypted = decipher.update(encrypted, 'hex', 'utf8')
 // 返回解密后的字符串 utf8编码
-decrypted = decipher.final('utf8')
+decrypted += decipher.final('utf8')
 
 console.log('Decrypted data:', decrypted)
 
